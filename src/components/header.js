cd575af6a016a5056e9c6fa888c96f6b92688a4a
Ryan McNair <ryan.mcnair@gmail.com>
2021-03-02 16:47:00 -0600

    updated content and added styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import EIcon from '../assets/images/email icon.png';
+import GitIcon from '../assets/images/GitIcon.png';
+import LinkedIn from '../assets/images/LinkedIn.png';
 
 export default class NavBar extends React.Component {
   state = {
@@ -17,7 +19,9 @@ export default class NavBar extends React.Component {
             <a href='./index.html#music'>Art & Music</a>
           </h4>
           <h5>
-            <a href='mailto: ryan.mcnair@gmail.com'><img className='email-icon' src={EIcon} alt='email icon' /></a>
+            <a href='mailto: ryan.mcnair@gmail.com'><img className='email-icon' src={EIcon} alt='email icon' /></a> ||{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/ryanmcnair"><img className='header-icon' src={GitIcon} alt='Github icon'/></a> ||{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ryan-mcnair-profile/"><img className='header-icon' src={LinkedIn} alt='LinkedIn icon'/></a>
           </h5>
         </header>
       </>
